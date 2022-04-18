8dd0754c637b683a1e5d523ca72e98e714349833
devanshu75 <devanshurana75@gmail.com>
2022-04-18 19:07:05 +0530

    button issue solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,28 @@ import { ConverterService } from './services/converter.service';
 })
 export class AppComponent implements OnInit {
 
-  public amount:number = 1;
-  public data:any;
-  public valueFrom:any = 'usd';
-  public valueTo:any = 'inr';
-  public result:any;
-  public constructor(private converterService: ConverterService) {}
+  public amount: number = 1;
+  public data: any;
+  public valueFrom: any = 'usd';
+  public valueTo: any = 'inr';
+  public result: any;
+
+  public constructor(private converterService: ConverterService) { }
 
   ngOnInit(): void {
-  
+
   }
 
-  convertCurrency(){
-    
-    this.converterService.convert(this.valueFrom,this.valueTo).subscribe(data => {
+  convertCurrency() {
+
+    let dataFrom = this.valueFrom;
+    let dataTo = this.valueTo;
+
+    this.converterService.convert(dataFrom, dataTo).subscribe(data => {
       this.data = Object.entries(data).map(x => x[1])
-      this.valueFrom = Object.entries(this.data[4]).map(x => x[1])[0]
-      this.valueTo = Object.entries(this.data[4]).map(x => x[1])[1]
-      this.result = (this.amount * (this.valueTo/this.valueFrom))
+      dataFrom = Object.entries(this.data[4]).map(x => x[1])[0]
+      dataTo = Object.entries(this.data[4]).map(x => x[1])[1]
+      this.result = (this.amount * (dataTo / dataFrom))
       console.log(this.result)
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
@@ -12,8 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    FormsModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
